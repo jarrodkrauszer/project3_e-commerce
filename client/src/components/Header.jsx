78db85da078e2ed35fc0f5583b81bba8692c7a3d
brandonom <brandonomyers1@gmail.com>
2023-11-16 15:06:20 -0500

    refactored header and carousel images

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import "../styles/header.scss";
-import Githublogo from "../assets/25231.png";
+
 import React from "react";
 
 function Header(propsObj) {
@@ -7,12 +7,12 @@ function Header(propsObj) {
     <header className="fixed-top d-flex justify-content-between">
       <nav>
         <a href="#">Home</a>
-        <a href="#">About Me</a>
-        <a href="#">Contact Me</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
         <a href="#">Projects</a>
       </nav>
     </header>
   );
 }
 
-export default MainHeader;
+export default Header;
