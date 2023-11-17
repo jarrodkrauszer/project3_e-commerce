b0364e915bde2ba76d7a55244c0e511abe7bd712
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-11-17 18:27:31 -0500

    Hero first draft

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -12,19 +12,6 @@ import { QUERY_PRODUCTS, QUERY_ALL_PRODUCTS } from "../../utils/queries";
 
 import "./category.scss";
 
-const QUERY_PRODUCTS = gql`
-  query Products($category: ID, $name: String) {
-    products(category: $category, name: $name) {
-      _id
-      name
-      category {
-        _id
-        name
-      }
-    }
-  }
-`;
-
 const Category = () => {
   const [category, setCategory] = useState(null);
   const { loading, error, data } = useQuery(QUERY_PRODUCTS, {
@@ -40,13 +27,13 @@ const Category = () => {
 
   return (
     <div className="flex flex-col items-center py-3 bg-gray-700">
-      <div className="category-container mx-10 h-80">
+      <div className="flex-container category-container ">
         <div className="flex-container">
           <div>
             <img
               src={Hat}
               alt="Hat"
-              className="object-contain w-full h-full px-1 transition duration-300 ease-in-out hover:brightness-75"
+              className="object-contain w-full h-full px-1 transition duration-300 ease-in-out hover:brightness-75 "
               onClick={() => handleCategoryClick("hat")}
             />
           </div>
@@ -54,36 +41,35 @@ const Category = () => {
             <img
               src={Jacket}
               alt="Jacket"
-              className="w-full h-full px-1 transition duration-300 ease-in-out hover:brightness-75"
+              className="px-1 transition duration-300 ease-in-out hover:brightness-75 "
               onClick={() => handleCategoryClick("jacket")}
             />
           </div>
         </div>
 
-        <div className="flex-container">
+        <div className="flex-container w-full h-full">
           <img
             src={Men}
             alt="Men"
-            className="w-full h-full px-1 transition duration-300 ease-in-out hover:brightness-75"
+            className="px-1 w-full transition duration-300 ease-in-out hover:brightness-75 "
             onClick={() => handleCategoryClick("men")}
           />
         </div>
       </div>
-      <div className="category-container mx-10">
-        <div className="flex-container">
+      <div className="flex-container category-container ">
+        <div className="flex-container w-full h-full">
           <img
             src={Women}
             alt="Women"
-            className="w-full h-full px-1 transition duration-300 ease-in-out hover:brightness-75"
+            className=" px-1 transition duration-300 ease-in-out hover:brightness-75 "
             onClick={() => handleCategoryClick("women")}
           />
         </div>
 
-        {/* New Row for the last two items */}
         <div className="flex-container">
           <img
             src={Sneakers}
-            className="w-full h-full px-1 transition duration-300 ease-in-out hover:brightness-75 hover:scale-105"
+            className="px-1 transition duration-300 ease-in-out hover:brightness-75 "
             alt="Sneakers"
             onClick={() => handleCategoryClick("sneakers")}
           />
