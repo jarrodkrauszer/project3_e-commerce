973aa570fd1468fb35c1a099fb1b5233a7bdd0d7
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-11-19 14:00:41 -0500

    cart styling

---
+++ server/utils/index.js
@@ -0,0 +1,13 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+};
