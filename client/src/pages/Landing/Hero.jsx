c8db10315631edd478e66db9bc2c6b7b87b23f16
alex pohlman <apohlman53@gmail.com>
2023-11-20 15:54:20 -0500

    added click functionality to carousel images. currently they arent routing correctly

--- client/src/pages/Landing/Hero.jsx
+++ client/src/pages/Landing/Hero.jsx
@@ -1,11 +1,24 @@
 import { useStoreContext } from "../../utils/store";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import { NavLink } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
-import Logo from "../../assets/logo.png";
+
+import { useQuery } from "@apollo/client";
+import { QUERY_CATEGORIES } from "../../utils/queries";
+import { UPDATE_CURRENT_CATEGORY } from "../../utils/actions";
 
 function Hero() {
   const [state, dispatch] = useStoreContext();
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const navigation = categoryData?.categories || [];
+
+  const handleClick = (id) => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id,
+    });
+    console.log("triggered");
+  };
   const imageSize = {
     width: "100%",
     height: "550px",
@@ -40,29 +53,49 @@ function Hero() {
 
   return (
     <div>
-
       <img
-        style={{ filter: 'invert(1)' }}
+        style={{ filter: "invert(1)" }}
         className="mx-auto h-15 w-auto invert-colors"
-        src={`/images/logo.png`} />
+        src={`/images/logo.png`}
+      />
       <h1 style={titleStyle}>UrbanVogue</h1>
 
-      <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false}>
-
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+      >
         <div>
-          <img src={`/images/white-background.png`} style={imageSize} />
+          <NavLink
+            to="/products"
+            onClick={() => handleClick(navigation[0]._id)}
+          >
+            <img src={`/images/white-background.png`} style={imageSize} />
+          </NavLink>
           <p className="legend" style={legendStyle}>
             70% Sale on all Menswear
           </p>
         </div>
         <div>
-          <img src={`/images/black-background.png`} style={imageSize} />
+          <NavLink
+            to="/products"
+            onClick={() => handleClick(navigation[4]._id)}
+          >
+            <img src={`/images/black-background.png`} style={imageSize} />
+          </NavLink>
           <p className="legend" style={altLegendStyle}>
             50% Sale on all Shoes
           </p>
         </div>
         <div>
-          <img src={`/images/white-background.png2`} style={imageSize} />
+          <NavLink
+            to="/products"
+            onClick={() => handleClick(navigation[2]._id)}
+            className="relative block h-full w-full rounded-lg overflow-hidden hover:opacity-75 transition-opacity duration-300"
+          >
+            <img src={`/images/white-background.png2`} style={imageSize} />
+          </NavLink>
           <p className="legend" style={legendStyle}>
             20% off all Hats
           </p>
