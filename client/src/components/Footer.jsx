8232405efa0fb84c215b31dc314b5b15a31a6e7b
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-11-20 11:39:26 -0500

    Adjusted footer format

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,31 +3,28 @@ import Logo from "../assets/logo.png";
 function Footer() {
   return (
     <footer className="w-full bg-black p-8 mt-14">
-      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-black text-center md:justify-between ">
-        <img src={Logo} alt="logo-ct" className="w-10" />
-        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
-          <li>
-            <a
-              href="#"
-              className="text-white font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
-            >
-              About Us
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-white font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
-            >
-              Contact Us
-            </a>
-          </li>
-        </ul>
+      <div className="flex items-center justify-center ">
+        <img src={Logo} alt="logo-ct" className="w-10 justify-center" />
       </div>
       <hr className="my-8 border-blue-gray-50" />
-      <p className="text-center text-white font-normal">
-        &copy; 2023 Urban Vogue
-      </p>
+      <div className="text-white flex flex-col md:flex-row justify-between items-start mx-auto">
+        {/* Contact Information */}
+        <div className="mb-4 md:mb-0">
+          <h3 className="underline">Contact Us:</h3>
+          <ul>
+            <li>Phone: (555)-657-8965</li>
+            <li>Email: urbanvogue@gmail.com</li>
+            <li>Address: 555 Deer Rd, Moonview, AK, 55555</li>
+          </ul>
+        </div>
+
+        {/* Copyright */}
+        <div className="mt-20">
+          <p className="text-white italic font-normal mt-4 md:mt-0">
+            &copy; 2023 Urban Vogue
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
