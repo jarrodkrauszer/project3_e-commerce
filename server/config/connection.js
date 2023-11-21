f77b9c35bf39a140e030db1d2d500e4245dfd422
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-11-21 09:26:49 -0500

    made edit to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/clothing_store_db');
+mongoose.connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/clothing_store_db');
 
 module.exports = mongoose.connection;
