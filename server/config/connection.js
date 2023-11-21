dab2669ba21b0078898039c331d68cd3bc62dfc5
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-11-21 09:59:10 -0500

    fixed deploy issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jk:password123AGAIN@cluster0.u1xl7wy.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/clothing_store_db');
+mongoose.connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/clothing_store_db');
 
 module.exports = mongoose.connection;
